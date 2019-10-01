a4cbdaaaf3a948190cc2aa23114bab37e51a198a
Bitbucket Pipelines <khpipelines@khipu.com>
2019-10-01 20:10:34 +0000

    Version 1.1.1

--- lib/khipu-chaski-api-client/api/device_registry_api.rb
+++ lib/khipu-chaski-api-client/api/device_registry_api.rb
@@ -59,6 +59,57 @@ module KhipuChaski
       return result
     end
 
+    # Registro de dispositivo eliminando alias anteriores
+    # Registra un dispositivo identificado por su tokenId, elimina alias anteriores
+    # @param device Dispositivo a registrar
+    # @param [Hash] opts the optional parameters
+    # @return [SuccessResponse]
+    def add_unique_alias_device(device, opts = {})
+      if Configuration.debugging
+        Configuration.logger.debug "Calling API: DeviceRegistryApi#add_unique_alias_device ..."
+      end
+      
+      # verify the required parameter 'device' is set
+      fail "Missing the required parameter 'device' when calling add_unique_alias_device" if device.nil?
+      
+      # resource path
+      path = "/devices/uniqueAlias".sub('{format}','json')
+
+      # query parameters
+      query_params = {}
+
+      # header parameters
+      header_params = {}
+
+      # HTTP header 'Accept' (if needed)
+      _header_accept = ['application/json']
+      _header_accept_result = @api_client.select_header_accept(_header_accept) and header_params['Accept'] = _header_accept_result
+
+      # HTTP header 'Content-Type'
+      _header_content_type = ['application/json']
+      header_params['Content-Type'] = @api_client.select_header_content_type(_header_content_type)
+
+      # form parameters
+      form_params = {}
+
+      # http body (model)
+      post_body = @api_client.object_to_http_body(device)
+      
+
+      auth_names = ['khipu']
+      result = @api_client.call_api(:POST, path,
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => 'SuccessResponse')
+      if Configuration.debugging
+        Configuration.logger.debug "API called: DeviceRegistryApi#add_unique_alias_device. Result: #{result.inspect}"
+      end
+      return result
+    end
+
     # Obtiene dispositivo
     # Obtiene la informacion de un dispositivo especifico
     # @param token_id Token que identifica al dispositivo. Tiene la forma &lt;platform&gt;:&lt;registryId&gt;, donde platform puede tomar los valores \&quot;and\&quot; o \&quot;ios\&quot;.
